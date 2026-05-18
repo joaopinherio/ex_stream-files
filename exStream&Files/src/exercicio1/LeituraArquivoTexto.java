3ed81a19cb0b39f407618a2e36cbd809974a123b
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2026-05-18 16:05:37 +0000

    base do codigo com o comeco da feature ex1

---
+++ exStream&Files/src/exercicio1/LeituraArquivoTexto.java
@@ -0,0 +1,22 @@
+import java.io.BufferedWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class LeituraArquivoTexto {
+    //acha o local/caminho do arquivo desejado
+    Path path = Paths.get("NOMES.TXT");
+
+    try{
+        //classe para "gravar" o arquivo
+        BufferedWriter bw = Files.newBufferedWriter(path, Charset.defaultCharset());
+        PrintWriter pw = new PrintWriter(bw);
+    }
+    catch(Exception e){
+        System.out.println(e);
+    }
+
+
+}
+
---
+++ exStream&Files/src/exercicio1/Main.java
@@ -0,0 +1,8 @@
+//package exercicio1;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
