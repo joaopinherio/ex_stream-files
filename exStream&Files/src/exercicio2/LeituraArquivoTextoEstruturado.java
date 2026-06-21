2dd0020d3cab7ab18430d5007984fac5dbeeca25
joaopinheiro <joaopdepaula@gmail.com>
2026-06-21 16:19:10 -0300

    read/write files

--- exStream&Files/src/exercicio2/LeituraArquivoTextoEstruturado.java
+++ exStream&Files/src/exercicio2/LeituraArquivoTextoEstruturado.java
@@ -9,28 +9,53 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class LeituraArquivoTextoEstruturado {
-    public LeituraArquivoTextoEstruturado(){
+    public LeituraArquivoTextoEstruturado() {
     }
-    
-    public void leArquivoTextoEstruturado(String nomeArquivo){
+
+    public void leArquivoTextoEstruturado(String nomeArquivo) {
         Path arq = Paths.get(nomeArquivo);
         BufferedReader reader = null;
         String line = "";
-        
-        try{
+
+        try {
             reader = Files.newBufferedReader(arq, Charset.defaultCharset());
-            while((line = reader.readLine()) != null){
-                String sb [] =  line.split(":");
+            while ((line = reader.readLine()) != null) {
+                String sb[] = line.split(":");
                 System.out.println("Numero: " + sb[0]);
-                System.out.println("Nome: " + sb[  1]);
+                System.out.println("Nome: " + sb[1]);
                 System.out.println("\n");
             }
             reader.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
-    
+
+    public void escreveArquivoTexto(String nomeArquivo, Scanner scanner){
+        Path arq = Paths.get(nomeArquivo);
+        Path out = Paths.get("Outuput.txt");
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+        String line = "";
+
+        try{
+            reader = Files.newBufferedReader(arq, Charset.defaultCharset());
+            writer = Files.newBufferedWriter(out, Charset.defaultCharset());
+
+            while((line = reader.readLine()) != null){
+                writer.append(line);
+                writer.append("\n");
+                String textoUser = scanner.nextLine();
+                writer.append(textoUser);
+            }
+            reader.close();
+            writer.close();
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
 }
--- exStream&Files/src/exercicio2/Main.java
+++ exStream&Files/src/exercicio2/Main.java
@@ -1,9 +1,14 @@
 package exercicio2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         LeituraArquivoTextoEstruturado ex2 = new LeituraArquivoTextoEstruturado();
         
         ex2.leArquivoTextoEstruturado("CADASTRO.TXT");
+
+        ex2.escreveArquivoTexto("CADASTRO.TXT", scanner);
     }
 }
